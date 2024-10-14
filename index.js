d7070b50b207a6b177e56043f7872d0d63ecf04d
Erik Wink <ew223pn@student.lnu.se>
2024-10-14 15:07:01 +0200

    renamed converter to fix capital letter

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { Converter } from './src/js/Converter.js'
+import { MainConverter } from './src/js/Converter.js'
 export { Converter }
-// adding a comment to test if name of Converter file changes when pushed
+
